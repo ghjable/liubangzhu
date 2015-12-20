edde6918eb20712af23fcd7d6ed026be0cf790f6
Yato_Kami <GHJ@abledeMacBook-Pro.local>
2015-12-20 22:07:47 +0800

    新建项目

---
+++ liubangzhu/liubangzhu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  liubangzhu
+//
+//  Created by able on 15/12/20.
+//  Copyright © 2015年 able. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ liubangzhu/liubangzhuTests/liubangzhuTests.m
@@ -0,0 +1,39 @@
+//
+//  liubangzhuTests.m
+//  liubangzhuTests
+//
+//  Created by able on 15/12/20.
+//  Copyright © 2015年 able. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface liubangzhuTests : XCTestCase
+
+@end
+
+@implementation liubangzhuTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
